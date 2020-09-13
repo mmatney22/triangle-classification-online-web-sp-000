4c8149301183794b5d0d21c087a4773f5f77085a
Morgan Matney <morgan.matney89@gmail.com>
2020-09-13 13:25:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+
+  attr_accessor :length_a, :length_b, :length_c
+
+  def initialize(length_a, length_b, length_c)
+    @length_a = length_a
+    @length_b = length_b
+    @length_c = length_c
+  end
+
+  def kind
+    #if triangle invalid
+      #begin
+        #raise TriangleError
+      #raise TriangleError => error
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
